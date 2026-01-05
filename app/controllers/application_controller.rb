dbd909b437a39f8bcda61ef954c322831cb8d3a6
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2026-01-05 09:21:48 +0000

    [build]DeviseのView生成とUserモデルのStrong Parameters設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # 新規登録時にnameカラムを許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # プロフィール更新時にnameカラムを許可
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end

2eed3ea9174ff20a5a92f91f8c45be5134c1e41a
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2026-01-20 08:55:07 +0000

    [Update] is_activeがfalseのユーザーのログインを制限する機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :check_user_status
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
 
+  protected
   def configure_permitted_parameters
     # 新規登録時にnameカラムを許可
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -11,7 +12,13 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  
+  def check_user_status
+    if user_signed_in? && !current_user.is_active
+      sign_out current_user
+      redirect_to root_path, alert: "このアカウントは現在ご利用いただけません。"
+    end
+  end
+
   # 退会（アカウント削除）後の遷移先を指定
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   
   validates :name, presence: true, length: { maximum: 20 }
+
+  # Deviseの認証時に呼ばれるメソッド
+  def active_for_authentication?
+    # is_activeがtrue、かつ管理者から凍結されていない場合にログインを許可
+    super && self.is_active
+  end
+
+  # ログイン拒否時のエラーメッセージを指定
+  def inactive_message
+    self.is_active ? super : :withdraw # :withdraw は後で辞書ファイルに追加
+  end
 end

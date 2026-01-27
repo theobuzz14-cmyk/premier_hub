adf3d4eb9653545c8bc8d08232de9233c7a9d69f
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2026-01-27 17:45:20 +0000

    [Update]Deviseのレイアウト調整、マイページ退会ボタンの仕様変更（withdrawに）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,23 @@ class UsersController < ApplicationController
     end
     @posts = @user.posts.order(created_at: :desc)
   end
+
+  def withdraw
+    # collection で定義しているので、操作対象は常に本人
+    @user = current_user
+    
+    # 管理者の場合（管理者が誤って自信を削除しないように念のため）
+    if @user.admin?
+      redirect_to mypage_users_path, alert: "管理者はマイページから退会できません。"
+      return
+    end
+
+    # 論理削除（ステータス更新）
+    if @user.update(is_active: false)
+      reset_session # ログインセッションをクリア（ログアウト状態にする）
+      redirect_to root_path, notice: "退会処理が完了しました。ご利用ありがとうございました。"
+    else
+      redirect_to mypage_users_path, alert: "退会処理に失敗しました。"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show] do
     collection do
+      patch 'withdraw'
       get 'mypage'
     end
   end
